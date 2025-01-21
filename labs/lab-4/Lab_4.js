71ae4d2ac51a67c01f35f7f1a522c30a4abda822
tuyennguyenlt-katalon <tuyen.lnguyen@katalon.com>
2025-01-21 13:37:11 +0700

    Add Lab 4

--- labs/lab-4/Lab_4.js
+++ labs/lab-4/Lab_4.js
@@ -1,22 +1,22 @@
 /*
-Creating 1 object representing Bank Count Number with below structure:
-
-{
-    accountNumber: String,
-    routingNumber: String,
-    balance: Number
-}
-. Creating another Bank Account Object which clone from the previous one and update the accountNumber only . Put them into ann array call: bankAccounts
-
-. Write a consolse interface app to:
-
-allow user to find account by using accountNumber, if the account is existing, print the name and balance info out (NOT all the object)
-allow user to withdraw the money and avoid user to take more than current balance
-Note: Strongly recommend to create functions to process each tasks
-=== Banking application===
-1. Find an account
-2. Update balance
-0. Exit the program
+*Creating 1 object representing Bank Count Number with below structure:
+*
+*{
+*    accountNumber: String,
+*    routingNumber: String,
+*    balance: Number
+*}
+*. Creating another Bank Account Object which clone from the previous one and update the accountNumber only . Put them into ann array call: bankAccounts
+*
+*. Write a consolse interface app to:
+*
+*allow user to find account by using accountNumber, if the account is existing, print the name and balance info out (NOT all the object)
+*allow user to withdraw the money and avoid user to take more than current balance
+*Note: Strongly recommend to create functions to process each tasks
+*=== Banking application===
+*1. Find an account
+*2. Update balance
+*0. Exit the program
 */
 
 const readline = require('readline-sync');
@@ -63,6 +63,10 @@ function printAndStoreBankingApplication() {
     return Number(readline.question('Enter your option: '));
 }
 
+/**
+ * Prints the update balance menu and returns the user's selected option.
+ * @returns {number} The user's selected option.
+ */
 function printAndStoreUpdateBalanceOption(){
     console.log(`=== Update Balance Option ===
         1. Withdraw

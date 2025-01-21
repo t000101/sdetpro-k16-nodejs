53b0ab5ea4c785706ed5abdb6ed9becefb936bad
tuyennguyenlt-katalon <tuyen.lnguyen@katalon.com>
2025-01-21 11:33:31 +0700

    Add Lab 4

--- labs/lab-4/Lab_4.js
+++ labs/lab-4/Lab_4.js
@@ -1,3 +1,24 @@
+/*
+Creating 1 object representing Bank Count Number with below structure:
+
+{
+    accountNumber: String,
+    routingNumber: String,
+    balance: Number
+}
+. Creating another Bank Account Object which clone from the previous one and update the accountNumber only . Put them into ann array call: bankAccounts
+
+. Write a consolse interface app to:
+
+allow user to find account by using accountNumber, if the account is existing, print the name and balance info out (NOT all the object)
+allow user to withdraw the money and avoid user to take more than current balance
+Note: Strongly recommend to create functions to process each tasks
+=== Banking application===
+1. Find an account
+2. Update balance
+0. Exit the program
+*/
+
 const readline = require('readline-sync');
 
 let firstAccountNumber = {
@@ -6,40 +27,48 @@ let firstAccountNumber = {
     accountBalance: 1000,
 }
 
-let secondAccountNumber = {...firstAccountNumber, accountNumber: 987654321}
+let secondAccountNumber = { ...firstAccountNumber, accountNumber: 987654321 };
 
-let arrayOfAccounts = [firstAccountNumber, secondAccountNumber]
+let arrayOfAccounts = [firstAccountNumber, secondAccountNumber];
 
-printBankingApplication();
+let optionBankApp = printAndStoreBankingApplication();
 
-let option = Number(readline.question('Enter your option: '));
-
-while (option !=0 ){
-    switch (option) {
+while (optionBankApp !== 0) {
+    switch (optionBankApp) {
         case 1:
             findAccount(arrayOfAccounts);
-            printBankingApplication()
+            optionBankApp = printAndStoreBankingApplication();
             break;
         case 2:
             updateAccount(arrayOfAccounts);
-            printBankingApplication()
-            break;
-        case 0:
-            console.log('Exiting the program ...');
+            optionBankApp = printAndStoreBankingApplication();
             break;
         default:
-            console.log('Invalid option!');
-            printBankingApplication()
-            break;
+            console.log('Invalid option! Please reselect: ');
+            optionBankApp = printAndStoreBankingApplication();
     }
-    option = Number(readline.question('Enter your option: '));
 }
 
-function printBankingApplication() {
+console.log('Exiting the program ...');
+
+/**
+ * Prints the banking application menu and returns the user's selected option.
+ * @returns {number} The user's selected option.
+ */
+function printAndStoreBankingApplication() {
     console.log(`=== Banking Application===
     1. Find an account
     2. Update balance
     0. Exit the program`);
+    return Number(readline.question('Enter your option: '));
+}
+
+function printAndStoreUpdateBalanceOption(){
+    console.log(`=== Update Balance Option ===
+        1. Withdraw
+        2. Deposit
+        0. No! And back to Bank Application menu`);
+        return Number(readline.question('Enter your option: '));
 }
 
 
@@ -48,7 +77,7 @@ function findAccount(arrayOfAccounts) {
     let account = arrayOfAccounts.find(account => account.accountNumber === accountNumber);
 
     if (account) {
-        const {accountHolder, accountBalance } = account;
+        const { accountHolder, accountBalance } = account;
         console.log(`Account holder: ${accountHolder}`);
         console.log(`Account balance: ${accountBalance}`);
     } else {
@@ -58,45 +87,56 @@ function findAccount(arrayOfAccounts) {
 }
 
 function updateAccount(arrayOfAccounts) {
+    let continueUpdate = true;
     let account = findAccount(arrayOfAccounts);
-    console.log(`=== Update Balance Option===
-        1. Withdraw
-        2. Deposit
-        0. Exit the program`);
+    let option =  printAndStoreUpdateBalanceOption();
+    while (continueUpdate) {
+        if (account) {         
+            switch (option) {
+                case 1:
+                    withdraw(account);
+                    option = printAndStoreUpdateBalanceOption()
+                    break;
+                case 2:
+                    deposit(account);
+                    option = printAndStoreUpdateBalanceOption()
+                    break;
+                case 0:
+                    console.log('Exiting the program ...');
+                    continueUpdate = false;
+                    break;
+                default:
+                    console.log('Invalid Update Balance Option!');
+                    continueUpdate = false;
+                    break;
+            }
+        } else {
+            console.log(`=== Do you want to continue update balance? ===
+                1. Yes
+                0. No! And back to Bank Application menu`);
+            let option = Number(readline.question('Enter your option: '));
+            if (option == 0) {
+                continueUpdate = false;
+            }
+        }
 
-    
-    let option = Number(readline.question('Enter your option: '));
-
-    switch (option) {
-        case 1:
-            withdraw(account);
-            break;
-        case 2:
-            deposit(account);
-            break;
-        case 0:
-            console.log('Exiting the program ...');
-            break;
-        default:
-            console.log('Invalid Update Balance Option!');
-            break;
     }
 }
 
-function withdraw({accountBalance}) {
+function withdraw(account) {
     let amount = Number(readline.question('Enter amount to withdraw: '));
 
-    if (accountBalance < amount) {
+    if (account.accountBalance < amount) {
         console.log('Insufficient balance!');
     } else {
-        accountBalance -= amount;
-        console.log(`Withdraw successful! Remaining balance: ${accountBalance}`);
+        account.accountBalance -= amount;
+        console.log(`Withdraw successful! Remaining balance: ${account.accountBalance}`);
     }
 }
 
-function deposit({accountBalance}) {
+function deposit(account) {
     let amount = Number(readline.question('Enter amount to deposit: '));
 
-    accountBalance += amount;
-    console.log(`Deposit successful! New balance: ${accountBalance}`);
+    account.accountBalance += amount;
+    console.log(`Deposit successful! New balance: ${account.accountBalance}`);
 }
